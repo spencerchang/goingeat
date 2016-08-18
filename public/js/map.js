1c365ae50376e39a50b7acf198e51ef11b1e3c3d
SpencerChang <spencerchang@users.noreply.github.com>
2016-08-18 18:20:30 +0800

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -46,6 +46,9 @@
 //}
 
 var neighborhoods = [{
+	lat: 25.034127,
+	lng: 121.564499
+}, {
 	lat: 52.511,
 	lng: 13.447
 }, {
@@ -64,14 +67,28 @@ var map;
 var show_count = 0;
 
 function initMap() {
+	//	初始設定
 	map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 12,
-		scrollwheel: false,
-		center: {
-			lat: 52.520,
-			lng: 13.410
-		}
+		scrollwheel: false
 	});
+
+	// Try HTML5 geolocation.
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+
+			map.setCenter(pos);
+		}, function() {
+			alert('地理定位失敗!');
+		});
+	} else {
+		// Browser doesn't support Geolocation
+		alert('您的瀏覽器不支援地理定位');
+	}
 
 	drop();
 }
@@ -85,6 +102,7 @@ function drop() {
 
 function addMarkerWithTimeout(position, timeout) {
 	var show_info = [0];
+
 	window.setTimeout(function() {
 		var marker = new google.maps.Marker({
 			position: position,
@@ -93,39 +111,24 @@ function addMarkerWithTimeout(position, timeout) {
 		});
 		markers.push(marker);
 
-		var contentString = '<div id="content">' +
-			'<div id="siteNotice">' +
-			'</div>' +
-			'<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-			'<div id="bodyContent">' +
-			'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-			'sandstone rock formation in the southern part of the ' +
-			'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-			'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-			'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-			'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-			'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-			'Aboriginal people of the area. It has many springs, waterholes, ' +
-			'rock caves and ancient paintings. Uluru is listed as a World ' +
-			'Heritage Site.</p>' +
-			'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-			'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-			'(last visited June 22, 2009).</p>' +
-			'</div>' +
-			'</div>';
+		var contentString = getInfoContent('', '');
 
+		// set up infowindow
 		var infowindow = new google.maps.InfoWindow({
 			content: contentString
 		});
-		
-		if($.inArray( show_count, show_info ) != -1){
+
+		// choose show item
+		if($.inArray(show_count, show_info) != -1) {
 			infowindow.open(map, marker);
 		}
-		
+
+		// listen click
 		marker.addListener('click', function() {
+			toggleBounce(marker);
 			infowindow.open(map, marker);
 		});
-		
+
 		show_count++;
 
 	}, timeout);
@@ -139,7 +142,7 @@ function clearMarkers() {
 	markers = [];
 }
 
-function toggleBounce() {
+function toggleBounce(marker) {
 	if(marker.getAnimation() !== null) {
 		marker.setAnimation(null);
 	} else {
@@ -147,6 +150,32 @@ function toggleBounce() {
 	}
 }
 
+function getInfoContent(img, word) {
+	var content = '<div id="content">' +
+		'<div id="siteNotice">' +
+		'</div>' +
+		'<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+		'<div id="bodyContent">' +
+		'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+		'sandstone rock formation in the southern part of the ' +
+		'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+		'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+		'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+		'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+		'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+		'Aboriginal people of the area. It has many springs, waterholes, ' +
+		'rock caves and ancient paintings. Uluru is listed as a World ' +
+		'Heritage Site.</p>' +
+		'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+		'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+		'(last visited June 22, 2009).</p>' +
+		'</div>' +
+		'</div>';
+
+	return content;
+}
+
+// 取Lat Lng
 function geocodeAddress(resultsMap, address) {
 	var geocoder = new google.maps.Geocoder();
 
